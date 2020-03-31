e28ce2697d6da6b174880edc79cd20af0d7f6f5e
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-03-31 14:47:26 +0700

    Mongoose's isNew Property

--- test/create_test.js
+++ test/create_test.js
@@ -2,10 +2,15 @@ const assert = require('assert')
 const User = require('../src/user')
 
 describe('Creating records', () => {
-  it('save a user', () => {
+  it('save a user', (done) => {
     const joe = new User({ name: 'Joe' })
 
     joe.save()
+      .then(() => {
+        // save successfully?
+        assert(!joe.isNew)
+        done()
+      })
   })
 })
 
