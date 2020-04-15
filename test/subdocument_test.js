350b8d7fe11f8f9422cd948c558d3ea8e38e9598
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-04-15 17:43:47 +0700

    Removing Subdocuments

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -36,4 +36,25 @@ describe('Subdocuments', () => {
         done()
       })
   })
+
+  it('can remove an exsiting subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Title' }]
+    })
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        const post = user.posts[0]
+        post.remove()
+        return user.save()
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0)
+        done()
+      })
+
+  })
 })
