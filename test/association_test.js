3e94c208d773b30dd4bdfcc0c82255e6eebf9b3c
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-04-21 16:15:52 +0700

    Loading Deeply Nested Associations

--- test/association_test.js
+++ test/association_test.js
@@ -24,11 +24,33 @@ describe('Association', () => {
 
   })
 
-  it.only('save a relation between a user and blogpost', (done) => {
+  it('save a relation between a user and blogpost', (done) => {
     User.findOne({ name: 'Joe' })
       .populate('blogPosts')
       .then((user) => {
         assert(user.blogPosts[0].title === 'JS is Great')
+        done()
+      })
+  })
+
+  it('saves a full relation graph', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe')
+        assert(user.blogPosts[0].title === 'JS is Great')
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on great post')
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe')
         done()
       })
   })
